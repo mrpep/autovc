5618b878aa9c8ab9681dd8204b108099aa253bc5
lpepino <leonardodpepino@gmail.com>
2020-11-18 17:40:57 -0300

    install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pip install gdown
+pip install wavenet_vocoder
+
+gdown --id 1SZPPnWAgpGrh0gQ7bXQJXXjOntbh4hmz
+gdown --id 1ORAeb4DlS_65WDkQN6LHx5dPyCM5PAVV
+gdown --id 1Zksy0ndlDezo9wclQNZYkGi_6i7zi4nQ
+
+touch __init__.py
+
+
